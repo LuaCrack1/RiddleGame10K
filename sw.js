8af39950b33e044aaec6a91336667017d627eb3b
LuaCrack <lenhatthanh2022tv@gmail.com>
2025-12-09 16:54:39 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('riddle-game').then(cache=>{
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/sw.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
